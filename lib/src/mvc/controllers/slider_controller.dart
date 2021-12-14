2dd6889c212d394e74f4dcfe9498c686f5c1d974
asadamatic <asadamatic@gmail.com>
2021-12-14 14:07:48 +0500

    Version 0.4 - Solving Issues
    - Dark mode enabled
    - Social Profile Links
    - GroceryList App embedded

--- lib/src/mvc/controllers/slider_controller.dart
+++ lib/src/mvc/controllers/slider_controller.dart
@@ -1,5 +1,3 @@
-import 'package:asadamatic/src/mvc/controllers/home_controller.dart';
-import 'package:boltgrocery/Screens/home_screen.dart';
 import 'package:boltgrocery/main.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:get/get.dart';
